278773b6589223233fb4e57d551ee7609cd2e3d0
fgnm <fgnm.dev@gmail.com>
2021-02-13 14:19:03 +0100

    Fix batch color in `TextureRegionDrawLogic`

--- src/main/java/games/rednblack/editor/renderer/systems/render/logic/TextureRegionDrawLogic.java
+++ src/main/java/games/rednblack/editor/renderer/systems/render/logic/TextureRegionDrawLogic.java
@@ -2,6 +2,7 @@ package games.rednblack.editor.renderer.systems.render.logic;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -11,34 +12,40 @@ import games.rednblack.editor.renderer.utils.ComponentRetriever;
 
 public class TextureRegionDrawLogic implements Drawable {
 
-	final private ComponentMapper<TintComponent> tintComponentComponentMapper;
-	final private ComponentMapper<TextureRegionComponent> textureRegionMapper;
-	final private ComponentMapper<TransformComponent> transformMapper;
-	final private ComponentMapper<DimensionsComponent> dimensionsComponentComponentMapper;
+    private final ComponentMapper<TintComponent> tintComponentComponentMapper;
+    private final ComponentMapper<TextureRegionComponent> textureRegionMapper;
+    private final ComponentMapper<TransformComponent> transformMapper;
+    private final ComponentMapper<DimensionsComponent> dimensionsComponentComponentMapper;
 
-	final private Vector2 atlasCoordsVector = new Vector2();
-	final private Vector2 atlasSizeVector = new Vector2();
+    private final Vector2 atlasCoordsVector = new Vector2();
+    private final Vector2 atlasSizeVector = new Vector2();
 
-	public TextureRegionDrawLogic() {
-		tintComponentComponentMapper = ComponentMapper.getFor(TintComponent.class);
-		textureRegionMapper = ComponentMapper.getFor(TextureRegionComponent.class);
-		transformMapper = ComponentMapper.getFor(TransformComponent.class);
-		dimensionsComponentComponentMapper = ComponentMapper.getFor(DimensionsComponent.class);
-	}
+    private final Color batchColor = new Color();
 
-	@Override
-	public void draw(Batch batch, Entity entity, float parentAlpha) {
+    public TextureRegionDrawLogic() {
+        tintComponentComponentMapper = ComponentMapper.getFor(TintComponent.class);
+        textureRegionMapper = ComponentMapper.getFor(TextureRegionComponent.class);
+        transformMapper = ComponentMapper.getFor(TransformComponent.class);
+        dimensionsComponentComponentMapper = ComponentMapper.getFor(DimensionsComponent.class);
+    }
+
+    @Override
+    public void draw(Batch batch, Entity entity, float parentAlpha) {
         TextureRegionComponent entityTextureRegionComponent = textureRegionMapper.get(entity);
         ShaderComponent shaderComponent = ComponentRetriever.get(entity, ShaderComponent.class);
 
         entityTextureRegionComponent.executeRefresh(entity);
 
-        if(entityTextureRegionComponent.polygonSprite != null && (shaderComponent == null || shaderComponent.renderingLayer == MainItemVO.RenderingLayer.SCREEN)) {
+        batchColor.set(batch.getColor());
+
+        if (entityTextureRegionComponent.polygonSprite != null && (shaderComponent == null || shaderComponent.renderingLayer == MainItemVO.RenderingLayer.SCREEN)) {
             drawTiledPolygonSprite(batch, entity);
         } else {
             drawSprite(batch, entity, parentAlpha);
         }
-	}
+
+        batch.setColor(batchColor);
+    }
 
     public void drawSprite(Batch batch, Entity entity, float parentAlpha) {
         TintComponent tintComponent = tintComponentComponentMapper.get(entity);
@@ -71,21 +78,19 @@ public class TextureRegionDrawLogic implements Drawable {
     }
 
     public void drawTiledPolygonSprite(Batch batch, Entity entity) {
-    	batch.flush();
+        batch.flush();
         TintComponent tintComponent = tintComponentComponentMapper.get(entity);
         TransformComponent entityTransformComponent = transformMapper.get(entity);
         TextureRegionComponent entityTextureRegionComponent = textureRegionMapper.get(entity);
-        
+
         DimensionsComponent dimensionsComponent = dimensionsComponentComponentMapper.get(entity);
-        float ppwu = dimensionsComponent.width/entityTextureRegionComponent.region.getRegionWidth();
+        float ppwu = dimensionsComponent.width / entityTextureRegionComponent.region.getRegionWidth();
         atlasCoordsVector.set(entityTextureRegionComponent.region.getU(), entityTextureRegionComponent.region.getV());
-        atlasSizeVector.set(entityTextureRegionComponent.region.getU2()-entityTextureRegionComponent.region.getU(), entityTextureRegionComponent.region.getV2()-entityTextureRegionComponent.region.getV());
+        atlasSizeVector.set(entityTextureRegionComponent.region.getU2() - entityTextureRegionComponent.region.getU(), entityTextureRegionComponent.region.getV2() - entityTextureRegionComponent.region.getV());
 
         batch.getShader().setUniformi("isRepeat", entityTextureRegionComponent.isRepeat ? 1 : 0);
         batch.getShader().setUniformf("atlasCoord", atlasCoordsVector);
-    	batch.getShader().setUniformf("atlasSize", atlasSizeVector);
-        //System.out.println(entityTransformComponent.originX);
-        //batch.setColor(tintComponent.color);
+        batch.getShader().setUniformf("atlasSize", atlasSizeVector);
         entityTextureRegionComponent.polygonSprite.setColor(tintComponent.color);
         float originX = entityTransformComponent.originX * entityTransformComponent.scaleX / ppwu;
         float originY = entityTransformComponent.originY * entityTransformComponent.scaleY / ppwu;
