c02ea37a249265062112348946d42762e6ff2343
fgnm <fgnm.dev@gmail.com>
2021-03-20 20:16:39 +0100

    Set minimum OpenGL version to 3.2

--- src/main/java/games/rednblack/editor/renderer/utils/DefaultShaders.java
+++ src/main/java/games/rednblack/editor/renderer/utils/DefaultShaders.java
@@ -3,7 +3,7 @@ package games.rednblack.editor.renderer.utils;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 public class DefaultShaders {
-    public static String DEFAULT_VERTEX_SHADER = "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
+    public static String DEFAULT_VERTEX_SHADER = "#version 100\n" + "attribute vec4 " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
             + "attribute vec4 " + ShaderProgram.COLOR_ATTRIBUTE + ";\n" //
             + "attribute vec2 " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
             + "uniform mat4 u_projTrans;\n" //
@@ -17,7 +17,7 @@ public class DefaultShaders {
             + "   v_texCoords = " + ShaderProgram.TEXCOORD_ATTRIBUTE + "0;\n" //
             + "   gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" //
             + "}";
-    public static String DEFAULT_FRAGMENT_SHADER = "#ifdef GL_ES\n" //
+    public static String DEFAULT_FRAGMENT_SHADER = "#version 100\n" + "#ifdef GL_ES\n" //
             + "#define LOWP lowp\n" //
             + "precision mediump float;\n" //
             + "#else\n" //
@@ -26,11 +26,8 @@ public class DefaultShaders {
             + "varying LOWP vec4 v_color;\n" //
             + "varying vec2 v_texCoords;\n" //
             + "uniform sampler2D u_texture;\n" //
-            //+ "uniform int wtf;\n" //
             + "void main()\n"//
             + "{\n" //
-            //TODO the most weird bug I've ever found!
             + "  gl_FragColor = v_color * texture2D(u_texture, v_texCoords);\n"
-            //+ "  if (wtf == 0) { gl_FragColor = v_color * texture2D(u_texture, v_texCoords); } else { gl_FragColor = vec4(0.0); }\n" //
             + "}";
 }
