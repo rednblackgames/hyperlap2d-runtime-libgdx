cda53e8292598302c077556f69da0dfdab0e5e4e
fgnm <fgnm.dev@gmail.com>
2023-10-10 12:50:16 +0200

    Apply again the bad fix to ignore thread errors in bitmap fonts

---
+++ src/main/java/games/rednblack/editor/renderer/utils/ThreadSafeBitmapFont.java
@@ -0,0 +1,74 @@
+package games.rednblack.editor.renderer.utils;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pools;
+
+public class ThreadSafeBitmapFont extends BitmapFont {
+    public ThreadSafeBitmapFont() {
+    }
+
+    public ThreadSafeBitmapFont(boolean flip) {
+        super(flip);
+    }
+
+    public ThreadSafeBitmapFont(FileHandle fontFile, TextureRegion region) {
+        super(fontFile, region);
+    }
+
+    public ThreadSafeBitmapFont(FileHandle fontFile, TextureRegion region, boolean flip) {
+        super(fontFile, region, flip);
+    }
+
+    public ThreadSafeBitmapFont(FileHandle fontFile) {
+        super(fontFile);
+    }
+
+    public ThreadSafeBitmapFont(FileHandle fontFile, boolean flip) {
+        super(fontFile, flip);
+    }
+
+    public ThreadSafeBitmapFont(FileHandle fontFile, FileHandle imageFile, boolean flip) {
+        super(fontFile, imageFile, flip);
+    }
+
+    public ThreadSafeBitmapFont(FileHandle fontFile, FileHandle imageFile, boolean flip, boolean integer) {
+        super(fontFile, imageFile, flip, integer);
+    }
+
+    public ThreadSafeBitmapFont(BitmapFontData data, TextureRegion region, boolean integer) {
+        super(data, region, integer);
+    }
+
+    public ThreadSafeBitmapFont(BitmapFontData data, Array<TextureRegion> pageRegions, boolean integer) {
+        super(data, pageRegions, integer);
+    }
+
+    @Override
+    public BitmapFontCache newFontCache() {
+        return new ThreadSafeBitmapFontCache(this, usesIntegerPositions());
+    }
+
+    static private class ThreadSafeBitmapFontCache extends BitmapFontCache {
+
+        public ThreadSafeBitmapFontCache(BitmapFont font) {
+            super(font);
+        }
+
+        public ThreadSafeBitmapFontCache(BitmapFont font, boolean integer) {
+            super(font, integer);
+        }
+
+        @Override
+        public GlyphLayout addText(CharSequence str, float x, float y, int start, int end, float targetWidth, int halign, boolean wrap, String truncate) {
+            //TODO this is a very bad workaroud that might not even work...
+            try {
+                return super.addText(str, x, y, start, end, targetWidth, halign, wrap, truncate);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Pools.obtain(GlyphLayout.class);
+            }
+        }
+    }
+}
