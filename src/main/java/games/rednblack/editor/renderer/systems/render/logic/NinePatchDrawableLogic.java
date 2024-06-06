8c620310cf170058dd42a133d0e4ac77acb61a71
fgnm <fgnm.dev@gmail.com>
2024-06-07 00:43:17 +0200

    Fix 9 patch tint color

--- src/main/java/games/rednblack/editor/renderer/systems/render/logic/NinePatchDrawableLogic.java
+++ src/main/java/games/rednblack/editor/renderer/systems/render/logic/NinePatchDrawableLogic.java
@@ -1,6 +1,7 @@
 package games.rednblack.editor.renderer.systems.render.logic;
 
 import com.artemis.ComponentMapper;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import games.rednblack.editor.renderer.components.DimensionsComponent;
 import games.rednblack.editor.renderer.components.NinePatchComponent;
@@ -14,18 +15,25 @@ public class NinePatchDrawableLogic implements DrawableLogic {
     protected ComponentMapper<DimensionsComponent> dimensionsMapper;
     protected ComponentMapper<NinePatchComponent> ninePatchMapper;
 
+    private final Color batchColor = new Color();
+
     @Override
     public void draw(Batch batch, int entity, float parentAlpha, RenderingType renderingType) {
+        batchColor.set(batch.getColor());
+
         TintComponent tintComponent = tintComponentComponentMapper.get(entity);
         TransformComponent entityTransformComponent = transformMapper.get(entity);
         DimensionsComponent entityDimensionsComponent = dimensionsMapper.get(entity);
         NinePatchComponent entityNinePatchComponent = ninePatchMapper.get(entity);
         batch.setColor(tintComponent.color);
+        batch.getColor().a *= parentAlpha;
 
         entityNinePatchComponent.ninePatch.draw(batch, entityTransformComponent.x, entityTransformComponent.y,
                 entityTransformComponent.originX, entityTransformComponent.originY,
                 entityDimensionsComponent.width, entityDimensionsComponent.height,
                 entityTransformComponent.scaleX, entityTransformComponent.scaleY, entityTransformComponent.rotation);
+
+        batch.setColor(batchColor);
     }
 
     @Override
